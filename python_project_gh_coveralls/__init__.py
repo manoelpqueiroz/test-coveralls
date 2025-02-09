27fba92b10a230a2732b0b7d37948093a18fe23d
Manoel Pereira de Queiroz <mpq.dev@pm.me>
2025-02-08 21:17:55 -0300

    :tada: Initial commit

---
+++ python_project_gh_coveralls/__init__.py
@@ -0,0 +1,13 @@
+# type: ignore[attr-defined]
+"""Awesome `python-project-gh-coveralls` is a Python cli/package created with https://gitlab.com/galactipy/galactipy."""
+
+from importlib import metadata
+
+from python_project_gh_coveralls.example import hello
+
+
+# Placeholder for poetry-dynamic-versioning, do not change:
+# https://github.com/mtkennerly/poetry-dynamic-versioning#installation
+__version__ = "0.0.0"
+
+__all__ = ['hello']
